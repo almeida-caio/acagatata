bc1e66aae338ef7562551d4a0415c0906826c821
almeida-caio <78153441+almeida-caio@users.noreply.github.com>
2021-01-29 15:05:28 -0300

    Including some visuals to main.js
    Applying minimum visual aspects to the simulation, showing the gene tree

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+// visuals are minimal, but working!
 let countMax = 3;
 let numChilds = 2;
 let parental = ["A", "C", "A", "G", "A", "T", "A", "T", "A"];
@@ -7,17 +8,40 @@ let counter;
 
 let auxChildGenes;
 
-// mutation algorithm is working fine!
-// must now implement the visuals...
+let step = 60;
+let step2 = 70;
+let edgeSize = 180;
+let angle = 3.14159 / 2.9;
+let deltaX, deltaY;
+let deltaXaux, deltaYaux;
 
 function setup() {
-	createCanvas(450, 450);
+	createCanvas(displayWidth, displayHeight);
 	background(0);
+	
+	translate(width / 6.2, height / 2.4);
 
+	fill(255);
+	noStroke();
+	textSize(20);
+	textAlign(CENTER, CENTER);
+	text(parental.join(""), -8, 0);
+
+	stroke(255);
+	strokeWeight(2);
+
+	deltaX = 0;
+	deltaY = 0;
 	counter = 0;
-	print(parental);
-	print("P");
+	tree();
 
+	fill(255);
+	noStroke();
+	textAlign(LEFT, CENTER);
+	
+	deltaX = 0;
+	deltaY = 0;
+	counter = 0;
 	breed(parental);
 }
 
@@ -65,15 +89,63 @@ function childsGenome(local_parent_genome) {
 
 function breed(genome) {
 	counter++;
+	let n = counter - 1;
 
 	for (var k = 1; k <= numChilds; k++) {
 		auxChildGenes = childsGenome(genome).slice(0);
-		print(auxChildGenes);
-		print("F" + counter.toString());
+		// print("F" + counter.toString());
+
+		deltaX = (edgeSize / counter**1.05) * cos(angle / counter**1.6);
+
+		deltaXaux = deltaX + (edgeSize / counter**1.4) * cos(angle / counter**1.2);
+		deltaYaux = deltaY + (edgeSize / counter**1.4) * sin(angle / counter**1.2);
+
+		if (k == 1) {
+			text(" " + auxChildGenes.join("") + " ", (step / (countMax + 1 - counter)) + (n + 1) * deltaXaux, (n + 1) * deltaYaux);
+		} else {
+			if (counter < countMax) {
+				text(" " + auxChildGenes.join("") + " ", (step / (countMax + 1 - counter)) + (n + 1) * deltaXaux, (n + 1) * deltaYaux);
+			} else {
+				text(" " + auxChildGenes.join("") + " ", (step / (countMax + 1 - counter)) + (n + 1) * deltaXaux, -(n + 1) * deltaYaux);
+			}
+		}
 
 		if (counter < countMax) {
+			push();
+			angle = -angle;
+			translate((n + 1) * deltaXaux, (n + 1) * deltaYaux);
 			breed(auxChildGenes);
 			counter -= 1;
+			pop();
+		}
+	}
+}
+
+function tree() {
+	counter++;
+	let n = counter - 1;
+
+	for (var k = 1; k <= numChilds; k++) {
+		deltaX = (edgeSize / counter*1.05) * cos(angle / counter**1.6);
+
+		deltaXaux = deltaX + (edgeSize / counter**1.4) * cos(angle / counter**1.2);
+		deltaYaux = deltaY + (edgeSize / counter**1.4) * sin(angle / counter**1.2);
+
+		if (counter == countMax) {
+			line((step2 * 1/(counter**0.3)) + n * deltaX, n * deltaY, (n + 1) * deltaXaux, (n + 1) * deltaYaux);
+			line((step2 * 1/(counter**0.3)) + n * deltaX, n * deltaY, (n + 1) * deltaXaux, -(n + 1) * deltaYaux);
+		} else {
+			line((step2 * 1/(counter**0.0001)) + n * deltaX, n * deltaY, (n + 1) * deltaXaux, (n + 1) * deltaYaux);
+			line((step2 * 1/(counter**0.0001)) + n * deltaX, n * deltaY, (n + 1) * deltaXaux, -(n + 1) * deltaYaux);
+		}
+
+		if (counter < countMax) {
+			push();
+			angle = -angle;
+			translate((n + 1) * deltaXaux, (n + 1) * deltaYaux);
+			tree();
+			counter -= 1;
+			pop();
 		}
 	}
 }
